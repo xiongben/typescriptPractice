dd4b47b3b3b1cb549412f3a72baf1f557c193a5f
xiongben <xiongben1993@gmail.com>
2020-10-03 17:04:58 +0800

    [add]

--- src/page/example3/example3.tsx
+++ src/page/example3/example3.tsx
@@ -53,6 +53,49 @@ function testTreeDemo(){
 
 }
 
-testTreeDemo()
+// testTreeDemo()
+
+function getLadyInfo({name,age}:{name:string,age:number}){
+    console.log("name: ",name)
+    console.log("age: ",age)
+}
+
+type Lady = {
+    name: string,
+    age: number
+}
+
+const arr1:{name:string,age:number}[] = []
+arr1[0] = {name: "xiaohong", age: 12}
+
+const arr2:Lady[] = []
+arr2[0] = {name: "xiaoming", age: 13}
+
+// console.log(arr1,arr2)
+
+interface Man{
+    name:string,
+    age?:number,
+    [propname:string]:any,
+    say():string
+}
+
+class Xiaoming implements Man{
+    name:string
+    age:number
+    score: number
+    constructor() {
+        this.name="xiaoming"
+        this.age = 22
+        this.score = 99
+    }
+
+    say():string{
+        return "hello!" + this.name
+    }
+}
+
+const xiaoming1 = new Xiaoming()
+console.log(xiaoming1.say())
 
 export default Example3
