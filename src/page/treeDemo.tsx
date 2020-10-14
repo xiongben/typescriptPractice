544bff237e9387d1f55d0cfff789cfe96642019e
xiongben <xiongben1993@gmail.com>
2020-10-14 18:22:48 +0800

    add

--- src/page/treeDemo.tsx
+++ src/page/treeDemo.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function TreeDemo(){
-    maxHeapDemo();
+    // maxHeapDemo();
+    radixSortDemo()
     return(
         <div>
             <h2>tree demo</h2>
@@ -55,6 +56,36 @@ function maxHeapDemo(){
      }
      return arr;
   }
+}
+//基数排序
+function radixSortDemo(){
+    let arr1 = [34,66,71,22,35,98,21,27,11,9,3,68,40,999,453,334];
+    var arr2= radixSort(arr1,3);
+    console.log(arr2);
+    function radixSort(arr:number[],maxDigit:number){
+        var count:number[][] = [];
+        var digit_before = 10;
+        var digit_after = 1;
+        for(var i=0;i<maxDigit;i++,digit_after *= 10,digit_before *= 10){
+            for(var j=0;j<arr.length;j++){
+                var digitNum = parseInt(String((arr[j]%digit_before)/digit_after)) ;
+                if(!count[digitNum]){
+                    count[digitNum]=[];
+                }
+                count[digitNum].push(arr[j]);
+            }
+            var temp =0;
+            for(var k=0;k<count.length;k++){
+               var val;
+               if(count[k] != null){
+                   while ((val = count[k].shift()) != null)
+                   arr[temp++] = val;
+               }
+            }
+        }
+        return arr;
+    }
+
 }
 
 
