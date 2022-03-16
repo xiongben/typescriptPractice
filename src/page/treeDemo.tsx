862cb4e7ff2f9816e90f6777a56332b50a8e6ff6
xiongben <xiongben1993@gmail.com>
2022-03-16 21:22:39 +0800

    [add]

--- src/page/treeDemo.tsx
+++ src/page/treeDemo.tsx
@@ -167,6 +167,7 @@ function TreeDemo(){
    //  countBitsDemo1()
    //  happyNumbeDemo1()
 //    binarySearchDemoNew()
+    // insertPositionDemoNew()
     
     return(
         <div>
@@ -174,6 +175,30 @@ function TreeDemo(){
         </div>
     )
 }
+
+//Search Insert Position
+function insertPositionDemoNew(){
+    let nums = [1,3,5,6], target = 2
+    console.log(searchInsert(nums, target))
+
+    function searchInsert(nums: number[], target: number): number {
+       let start = 0, end = nums.length - 1
+       if(nums[start] > target) return 0
+       if(nums[end] < target) return nums.length
+       let mid = 0
+       while (start < end) {
+        mid = (start + end) >> 1
+        if(nums[mid] == target) return mid
+        if(nums[mid] < target) {
+            start = mid + 1
+        }else{
+            end = end - 1;
+        }
+       }
+       return start
+    };
+}
+
 //simple binary search
 function binarySearchDemoNew(){
     let nums = [-1,0,3,5,9,12], target = 9
